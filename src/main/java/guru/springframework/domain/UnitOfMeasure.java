339fe4c0bbd1158c66dc9513ffb871cdb4f2bab5
Rick <tillett.eric@gmail.com>
2020-06-06 17:05:32 -0400

    Added UnitOfMeasure class with unidirectional relationship from Ingredient.

---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,33 @@
+package guru.springframework.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Set;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String description() {
+        return description;
+    }
+
+    public void description(String uom) {
+        this.description = uom;
+    }
+}
