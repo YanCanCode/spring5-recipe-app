c40de3d912bd2e2f8952020b54c6058376faca89
Rick <tillett.eric@gmail.com>
2020-06-25 20:25:38 -0400

    Added some helper methods to Ingredient to clean up RecipeLoader

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -126,6 +126,12 @@ public class Recipe {
         this.ingredients = ingredients;
     }
 
+    public Recipe addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+    }
+
     public Difficulty getDifficulty() {
         return difficulty;
     }
@@ -140,6 +146,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Set<Category> getCategories() {
